b0fb4efb4282fa24d1dff7cfdf97d3b53ded5dc2
faiz alkautsar <alkafaiz99@gmail.com>
2020-09-22 12:28:34 +0800

    fix smtp error

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 const { getStatus } = require("./getStatus");
 const { sendEmail } = require("./sendEmail");
 
-exports.main = async () => {
+exports.main = async (req, res) => {
   const response = await getStatus();
 
-  if (response.success && response.isUpdated) {
+  if (response.success && !response.isUpdated) {
     await sendEmail();
   }
 };
--- sendEmail.js
+++ sendEmail.js
@@ -4,19 +4,23 @@ exports.sendEmail = async () => {
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
+      type: "OAuth2",
       user: process.env.MYEMAIL,
-      pass: process.env.MYPW
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: process.env.ACCESS_TOKEN
     }
   });
 
   let info = await transporter.sendMail({
     from: '"MyEntry-WATCH" <foo@example.com>',
-    to: "alkafaiz99@gmail.com",
+    to: "faiz.bro99@gmail.com",
     subject: "MyEntry Status Update",
     text:
       "There has been a development in your MyEntry Application status, go check now!",
     html:
-      "<b>There has been a development in your MyEntry Application status, go check now!</b>"
+      "<b>There has been a development in your MyEntry Application status, go check now!</b><br/><em>computer generated notification developed by <span><a href='mailto:alkafaiz99@gmail.com'>alkafaiz</a></span>.<em/>"
   });
 
   console.log("Message sent: %s", info.messageId);
